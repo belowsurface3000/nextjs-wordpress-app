64108a9956e851fe18c767e61af20a1b0de81b30
belowsurfac3000 <fabian@besurelabel.de>
2022-02-18 19:34:34 +0100

    update apollo client

--- pages/tutorials.js
+++ pages/tutorials.js
@@ -31,8 +31,8 @@ export default function Tutorials(props) {
             <h1>Tutorials</h1>
             <div className="tutorial-card-container">
                 {tutorials.map((tutorial) => (
-                    <Link href={`tutorial/${tutorial.databaseId}`}>
-                        <div key={tutorial.databaseId} className="tutorial-card">
+                    <Link href={`tutorial/${tutorial.databaseId}`} key={tutorial.databaseId}>
+                        <div className="tutorial-card">
                             <h3>{tutorial.tutorialTitle}</h3>
                             {tutorial.tutorialPicture && <Image src={tutorial.tutorialPicture.mediaItemUrl} alt={tutorial.tutorialPicture.altText} width={350} height={250} />}
                         </div>
